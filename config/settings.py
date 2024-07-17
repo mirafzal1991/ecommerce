bbb5418b9a02317545c034abbfa07a272a15a699
mirafzal <mirobitovmirafzalpython@gmail.com>
2024-07-17 19:57:16 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -132,10 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'customer.User'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mirobitovmirafzalpython@gmail.com'
-EMAIL_HOST_PASSWORD = 'vvno rbmb yzhe mfnq'
+
 
